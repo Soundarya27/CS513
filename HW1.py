d649fdc7496cfa3a3791ac6365a3d03bca05ece4
Soundarya <shiriyannamatti@civisanalytics.com>
2020-02-19 16:59:20 -0600

    modified input params

--- HW1.py
+++ HW1.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, os
 import cv2
 import numpy as np
 import sys
@@ -12,28 +12,23 @@ def setup_parser():
     }
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--dir_name', type=str, help=help_text['dir_name'], required=True)
-    parser.add_argument('--image_count', type=int, help=help_text['image_count'], default='1')
+    parser.add_argument('--dir_name', type=dir_path, help=help_text['dir_name'], required=True)
+    parser.add_argument('--image_count', type=int, help=help_text['image_count'], required=True)
     return parser.parse_args()
 
-#def getDir():
-#    if len(sys.argv) == 3:
-#        dirctory = sys.argv[1]
-#        imageNum = int(sys.argv[2])
-#    else:
-#        print("Please input the Image file directory and image number.")
-#        sys.exit(1)
-
-#    print("smear auto detect started.")
-#    return dirctory, imageNum
+def dir_path(dirName):
+    if os.path.isdir(dirName):
+        return dirName
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{dirName} is not a valid path")
 
 def calMeanImg(dirctory, imageNum):
-    images = []
+    listImages = []
     for n in range(imageNum):
         img = listdir(dirctory)[n]
         imgDir = join(dirctory, img)
         if isfile(imgDir):
-            images.append(imgDir)
+            listImages.append(imgDir)
 
     print("Directory loaded.")
 
@@ -42,15 +37,11 @@ def calMeanImg(dirctory, imageNum):
 
     print("Calculating the mean gradient image of %s images..."% imageNum)
 
-    for n in images :
+    for n in listImages :
         currentImg = cv2.imread(n, 0)
-        # currentImg = gradient(currentImg)
         meanImg = meanImg + (currentImg / imageNum)
         del currentImg
     print("mean image created.")
-
-    # mdieanImg = listdir(dirctory)[2 / imageNum]
-    # medieanImgDir = join(dirctory, img)
 
     return meanImg
 
@@ -66,8 +57,6 @@ def save(image, outputName):
 
 def cvt2Gray(image):
     imageBW = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # showWin(image_bw, "gray")
-    # print("grayscale Image created")
     return imageBW
 
 def sobel(image):
@@ -77,7 +66,6 @@ def sobel(image):
 
 def smooth(image):
     smooth = cv2.GaussianBlur(image,(3,3),0)
-    # showWin(blur, "blur")
     return smooth
 
 def gradient(image):
@@ -85,42 +73,25 @@ def gradient(image):
     smoothImg = smooth(grayImg)
 
     sobelImg = sobel(smoothImg)
-    # print("gradient image created.")
     return sobelImg
 
 def createMask(image):
-    # image = cv2.normalize( image, image, 0, 255, cv2.NORM_MINMAX)
-    # cv2.imshow('NORMALimage',image)
     kernel = np.ones((5, 5),np.uint8)
-    # opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations = 3)
     dilated = cv2.dilate( image, kernel, iterations = 2)
     mask = cv2.erode(dilated,kernel,iterations = 2)
-
-    # mask = cv2.normalize( mask, mask, 0, 255, cv2.NORM_MINMAX)
 
     _, mask = cv2.threshold( mask, 100, 255,cv2.THRESH_BINARY)
     print( "Mask Image created.")
     return mask
 
 def main(setup_parser):
-#    dirctory, imageNum = getDir()
     meanImg = calMeanImg(setup_parser.dir_name, setup_parser.image_count)
-    # print(meanImg.dtype())
-    # cv2.normalize( meanImg, meanImg, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     save(meanImg, "meanImg.jpg")
-    # showWin(meanImg, "meanImg")
-    # gradientMeanImg = gradient(meanImg)
-    # save(gradientMeanImg, "gradientMeanImg.jpg")
-    # copyImg = meanImg.copy()
-    # gradientImg = gradient(meanImg)
-    # showWin(gradientImg, "gradientImg")
     meanImg = smooth(meanImg)
 
     mask = createMask(meanImg)
     showWin(mask, "mask")
     save(mask, "mask.jpg")
-
-
 
 if __name__ == '__main__':
     main(setup_parser())
